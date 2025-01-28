54a58bdfc4d75f4810de5f04fbf14d6c7d19c4ca
FabioMedeiros1000 <fabio.leandro.medeiros@gmail.com>
2025-01-28 10:56:21 -0300

    :truck: chore: renaming baseUrl to match remote server

--- src/servicos/favoritos.js
+++ src/servicos/favoritos.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const favoritosAPI = axios.create({baseURL: "http://localhost:8000/favoritos"})
+const favoritosAPI = axios.create({baseURL: "https://alura-books-server.onrender.com/favoritos"})
 
 async function getFavoritos() {
     const response = await favoritosAPI.get('/')
